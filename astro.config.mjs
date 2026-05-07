5ab086db1cbbc56ceba1e7c184415b243f424668
davidlsma <davidlede3@gmail.com>
2026-05-07 02:49:54 -0300

    cambio

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,10 @@ import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
   output: 'static',
+  base: '/vet365-landing',
+  build: {
+    assets: 'static',
+  },
   vite: {
     plugins: [tailwindcss()],
   },
